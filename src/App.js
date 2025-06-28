3a0d3ef7ede32f42faf59fc77f8e458982a9fd65
vanshika <vanshikavk4@gmail.com>
2025-06-28 18:22:06 +0530

    children Routing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -18,14 +19,22 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
   },
-  {
-    path: "/about",
-    element: <About />,
-  },{
-    path: "/contact",
-    element: <Contact />
-  }
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router = {appRouter} />);
+root.render(<RouterProvider router={appRouter} />);
